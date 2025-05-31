fe7216e51baf839f02c31e191f55fe4e9291c820
gabai62 <gabai62it@gmail.com>
2025-05-31 14:31:17 +0300

    v2

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -89,7 +89,7 @@ export const FindMovie: React.FC<Props> = ({ onAdd }) => {
 
           {error && (
             <p className="help is-danger" data-cy="errorMessage">
-              Can&apos;t find a movie with such a title
+              {error}
             </p>
           )}
         </div>
